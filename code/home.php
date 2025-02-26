f28d51c16d6c5cb77f117199a2af642a3f07c1dc
anditariz <anditarizkar@gmail.com>
2025-02-26 18:04:01 +0700

    update home

--- code/home.php
+++ code/home.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Bite | Me</title>
 
-    <!-- Boostrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"/>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-      <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <!-- Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-    <!-- CSS -->
-    <link rel="stylesheet" href="../css/style.css" />
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bite | Me</title>
 
-    <div class="header">
-      <div class="logo">Bite<span>|</span>ME</div>
-      <div class="ellipsis">
-        <i class="fas fa-ellipsis-v" id="sidebar"></i>
-      </div>
+  <!-- Boostrap CSS -->
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Tailwind CSS -->
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  <!-- CSS -->
+  <link rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
+
+  <div class="header">
+    <div class="logo">Bite<span>|</span>ME</div>
+    <div class="ellipsis">
+      <i class="fas fa-ellipsis-v" id="sidebar"></i>
     </div>
+  </div>
 
-    <div class="main-content">
-        <h1 class="main-tittle">H<i class="fas fa-cookie-bite"></i>MEMADE<br> COOKIE'S & CHIP'S</h1>
-        <button class="button">Scroll for More</button>
-    </div>
+  <div class="main-content">
+    <h1 class="main-tittle">H<i class="fas fa-cookie-bite"></i>MEMADE<br> COOKIE'S & CHIP'S</h1>
+    <button class="button">Scroll for More</button>
+  </div>
 
-    <div class="footer">
-        <a href="#" class="social-media"><i class="bi bi-linkedin"></i></a>
-        <a href="#" class="social-media"><i class="bi bi-youtube"></i></a>
-        <a href="#" class="social-media"><i class="bi bi-instagram"></i></a>
-    </div>
-  </body>
+  <div class="footer">
+    <a href="#" class="social-media"><i class="bi bi-linkedin"></i></a>
+    <a href="#" class="social-media"><i class="bi bi-youtube"></i></a>
+    <a href="#" class="social-media"><i class="bi bi-instagram"></i></a>
+  </div>
+</body>
+
 </html>
